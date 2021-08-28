a2b8f2e13f4c627e6b0a18465ad8acdb128599a4
Corine <corine@localhost.localdomain>
2021-08-29 00:00:32 +0100

    rbenv 4

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,42 +1,53 @@
 # config valid only for current version of Capistrano
 lock '3.6.0'
-
+# Application name to deploy
 set :application, 'awsblog'
-set :repo_url, 'https://github.com/rinouu/awsdeploy'
-set :rbenv_type, :user # or :system, or :fullstaq (for Fullstaq Ruby), depends on your rbenv setup
-#set :rbenv_ruby, '2.6.5'
-
-set :rbenv_prefix, "RBENV_ROOT=/usr/local/rbenv/  /usr/local/rbenv/bin/rbenv exec"
-#set :rbenv_prefix, "RBENV_VERSION=/usr/local/rbenv/ /usr/local/rbenv/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-set :rbenv_roles, :all # default value
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+# git repository to clone
+# (Xxxxxxxx: user name, yyyyyyyy: application name)
+set :repo_url, 'https://github.com/rinouu/awsdeploy'# deployするブランチ。デフォルトでmainを使用している場合、masterをmainに変更してください。
+set :branch, ENV['BRANCH'] || 'master'
+# The directory to deploy to.
+set :deploy_to, '/var/www/awsblog'
+# Folders/files with symbolic links
+set :linked_files, %w{.env config/secrets.yml}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
+# Number of versions to retain (*described later)
+set :keep_releases, 5
+# Ruby version
+set :rbenv_ruby, '2.6.5'
+set :rbenv_type, :system
+# The level of the log to output. Settings to: debug if you want to see the error log in detail.
+# For production environments,: info is normal。
+# However, if you want to check the behavior firmly, Settings it to: debug.
+set :log_level, :info
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+  desc 'Create database'
+  task :db_create do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:create'
+        end
+      end
+    end
+  end
+  desc 'Run seed'
+  task :seed do
+    on roles(:app) do
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
+  after :publishing, :restart
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    end
+  end
+end
